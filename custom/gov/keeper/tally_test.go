9f79ed9a255c0fc83d80d0b4a9db648be27a514a
larry <26318510+larry0x@users.noreply.github.com>
2022-07-09 03:04:18 +0100

    a minor cosmetic change

--- custom/gov/keeper/tally_test.go
+++ custom/gov/keeper/tally_test.go
@@ -9,7 +9,7 @@ import (
 
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
-	simapp "github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
