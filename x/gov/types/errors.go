1f776042d9d41a8b3981cc7633ead9b24bbc8782
faddat <jacobgadikian@gmail.com>
2022-07-11 06:06:41 +0000

    fix errors

--- x/gov/types/errors.go
+++ x/gov/types/errors.go
@@ -7,5 +7,5 @@ import (
 )
 
 // ErrFailedToQueryVesting represents an error where the gov module fails to query the vesting contract
-// NOTE: the vanilla gov module already registered 2-9, so we start from 10
-var ErrFailedToQueryVesting = sdkerrors.Register(govtypes.ModuleName, 10, "failed to query vesting contract")
+// NOTE: the vanilla gov module already registered 2-15, so we start from 16
+var ErrFailedToQueryVesting = sdkerrors.Register(govtypes.ModuleName, 16, "failed to query vesting contract")
