438a467dddda699171f6b379eea8b4827ec90884
larry <26318510+larry0x@users.noreply.github.com>
2022-07-24 23:11:03 +0100

    unit tests for incentives module

--- x/incentives/module_test.go
+++ x/incentives/module_test.go
@@ -1,1 +1,25 @@
 package incentives_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	marsapptesting "github.com/mars-protocol/hub/app/testing"
+
+	"github.com/mars-protocol/hub/x/incentives/types"
+)
+
+// TestCreatesModuleAccountAtGenesis asserts that the incentives module account is properly registered
+// with the auth module at genesis
+func TestCreatesModuleAccountAtGenesis(t *testing.T) {
+	app := marsapptesting.MakeMockApp()
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
+	require.NotNil(t, acc)
+}
