ed0016b3a27266dacd30605ea0898131886aec03
larry <26318510+larry0x@users.noreply.github.com>
2022-12-16 02:29:58 +0000

    update swagger

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -15,11 +15,23 @@
 
 set -eo pipefail
 
-tmp_dir="./tmp-swagger-gen"
+# The directory where the final output are to be stored
 docs_dir="./docs"
-proto_dirs=$(find ./proto ./third_party/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 
+# The directory where temporary swagger files are to be stored before they are
+# combined. Will be deleted in the end
+tmp_dir="./tmp-swagger-gen"
+if [ -d $tmp_dir ]; then
+  rm -rf $tmp_dir
+fi
 mkdir -p $tmp_dir
+
+# Directories that contain protobuf files that are to be transpiled into swagger
+# These include Mars modules and third party modules and services
+cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
+ibc_go_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/ibc-go/v4)
+wasmd_dir=$(go list -f '{{ .Dir }}' -m github.com/CosmWasm/wasmd)
+proto_dirs=$(find ./proto ${cosmos_sdk_dir}/proto ${ibc_go_dir}/proto ${wasmd_dir}/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 
 # Generate swagger files for `query.proto` and `service.proto`
 for dir in $proto_dirs; do
