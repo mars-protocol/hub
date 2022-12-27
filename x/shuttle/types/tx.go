ed5a9c58a028217154de4beb3f0d269de48ff29e
larry <26318510+larry0x@users.noreply.github.com>
2022-12-27 22:24:06 +0000

    implement Msg interface for MsgSendFunds

--- x/shuttle/types/tx.go
+++ x/shuttle/types/tx.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 var _ sdk.Msg = &MsgRegisterAccount{}
@@ -14,7 +15,7 @@ var _ sdk.Msg = &MsgRegisterAccount{}
 func (m *MsgRegisterAccount) ValidateBasic() error {
 	// the authority address must be valid
 	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
-		return ErrInvalidProposalAuthority.Wrap(err.Error())
+		return sdkerrors.Wrap(ErrInvalidProposalAuthority, err.Error())
 	}
 
 	return nil
@@ -22,6 +23,33 @@ func (m *MsgRegisterAccount) ValidateBasic() error {
 
 // GetSigners returns the expected signers for the message
 func (m *MsgRegisterAccount) GetSigners() []sdk.AccAddress {
+	// we have already asserted that the authority address is valid in
+	// ValidateBasic, so can ignore the error here
+	addr, _ := sdk.AccAddressFromBech32(m.Authority)
+	return []sdk.AccAddress{addr}
+}
+
+//------------------------------------------------------------------------------
+// MsgSendFunds
+//------------------------------------------------------------------------------
+
+// ValidateBasic does a sanity check on the provided data
+func (m *MsgSendFunds) ValidateBasic() error {
+	// the authority address must be valid
+	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
+		return sdkerrors.Wrap(ErrInvalidProposalAuthority, err.Error())
+	}
+
+	// the coins amount must be valid
+	if err := m.Amount.Validate(); err != nil {
+		return sdkerrors.Wrap(ErrInvalidProposalAmount, err.Error())
+	}
+
+	return nil
+}
+
+// GetSigners returns the expected signers for the message
+func (m *MsgSendFunds) GetSigners() []sdk.AccAddress {
 	// we have already asserted that the authority address is valid in
 	// ValidateBasic, so can ignore the error here
 	addr, _ := sdk.AccAddressFromBech32(m.Authority)
