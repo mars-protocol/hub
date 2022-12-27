7245e00624b45134fecdfe94317c0ac7f84e65d6
larry <26318510+larry0x@users.noreply.github.com>
2022-12-27 22:06:50 +0000

    implement SendFunds method

--- x/shuttle/keeper/msg_server.go
+++ x/shuttle/keeper/msg_server.go
@@ -2,15 +2,30 @@ package keeper
 
 import (
 	"context"
-	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	icacontrollertypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/controller/types"
 	icatypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	ibcclienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
 
 	"github.com/mars-protocol/hub/x/shuttle/types"
+)
+
+const (
+	// timeoutTime is the timeout time for ICS-20 packets.
+	//
+	// Currently we set this as a constant of 5 minutes. Should be make this a
+	// configurable parameter? Or a part of the proposal?
+	timeoutTime = 5 * time.Minute
+
+	// memo is the memo string to be attached to ICS-20 packets.
+	//
+	// TODO: think of something cool to use as memo
+	memo = ""
 )
 
 type msgServer struct{ k Keeper }
@@ -25,7 +40,6 @@ func (ms msgServer) RegisterAccount(goCtx context.Context, req *types.MsgRegiste
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	owner := ms.k.GetModuleAddress().String()
-
 	portID, err := icatypes.NewControllerPortID(owner)
 	if err != nil {
 		return nil, err
@@ -33,8 +47,8 @@ func (ms msgServer) RegisterAccount(goCtx context.Context, req *types.MsgRegiste
 
 	// there must not already be an interchain account associated with this
 	// connection id
-	if _, found := ms.k.icaControllerKeeper.GetInterchainAccountAddress(ctx, req.ConnectionId, portID); found {
-		return nil, sdkerrors.Wrapf(types.ErrAccountExists, "an interchain account already exists on %s", req.ConnectionId)
+	if address, found := ms.k.icaControllerKeeper.GetInterchainAccountAddress(ctx, req.ConnectionId, portID); found {
+		return nil, sdkerrors.Wrapf(types.ErrAccountExists, "an interchain account with address %s already exists on %s", address, req.ConnectionId)
 	}
 
 	// build the register interchain account message
@@ -59,5 +73,86 @@ func (ms msgServer) RegisterAccount(goCtx context.Context, req *types.MsgRegiste
 }
 
 func (ms msgServer) SendFunds(goCtx context.Context, req *types.MsgSendFunds) (*types.MsgSendFundsResponse, error) {
-	return nil, fmt.Errorf("UNIMPLEMENTED")
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	owner := ms.k.GetModuleAddress()
+	portID, err := icatypes.NewControllerPortID(owner.String())
+	if err != nil {
+		return nil, err
+	}
+
+	// query details of the transfer channel
+	//
+	// the objective is to find the connection id associated with the channel
+	channel, found := ms.k.channelKeeper.GetChannel(ctx, ibctransfertypes.PortID, req.ChannelId)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrChannelNotFound, "channel with port ID %s and channel ID %s does not exist", ibctransfertypes.PortID, req.ChannelId)
+	}
+
+	// the transfer channel must only have one hop
+	//
+	// we do not need to support multihop channels, as Mars Hub will establish
+	// direct connections with all its outpost chains.
+	if len(channel.ConnectionHops) > 1 {
+		return nil, sdkerrors.Wrapf(types.ErrMultihopUnsupported, "%s has more than one connection hops", req.ChannelId)
+	}
+
+	// find the interchain account address associated with the connection
+	connectionID := channel.ConnectionHops[0]
+	address, found := ms.k.icaControllerKeeper.GetInterchainAccountAddress(ctx, connectionID, portID)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrAccountNotFound, "no interchain account already exists on %s", connectionID)
+	}
+
+	// find token balances of the shuttle module account
+	balance := ms.k.bankKeeper.GetAllBalances(ctx, owner)
+
+	// if the proposal requires sending more coins than what the module acocunt
+	// holds, then draw the difference from the community pool
+	shortfall := saturateSub(req.Amount, balance)
+	if !shortfall.Empty() {
+		ms.k.distrKeeper.DistributeFromFeePool(ctx, shortfall, owner)
+	}
+
+	// send the funds via ICS-20
+	//
+	// because ICS-20 only supports one coin per packet, we need to dispatch a
+	// packet for each coin.
+	//
+	// the ibctransferkeeper has a sendTransfer method but it's not public.
+	// therefore we need to send a MsgTransfer to the baseapp msgRouter.
+	handler := ms.k.router.HandlerByTypeURL(sdk.MsgTypeURL(&ibctransfertypes.MsgTransfer{}))
+	for _, coin := range req.Amount {
+		msg := ibctransfertypes.NewMsgTransfer(
+			ibctransfertypes.PortID,
+			req.ChannelId,
+			coin,
+			owner.String(),
+			address,
+			ibcclienttypes.Height{}, // no timeout height. we use timeout timestamp instead
+			uint64(ctx.BlockTime().Add(timeoutTime).Unix()),
+			memo,
+		)
+		if _, err := handler(ctx, msg); err != nil {
+			return nil, err
+		}
+	}
+
+	ms.k.Logger(ctx).Info(
+		"initiated ICS-20 transfer(s) to interchain account",
+		"connectionID", connectionID,
+		"channelID", req.ChannelId,
+		"amount", req.Amount.String(),
+	)
+
+	return &types.MsgSendFundsResponse{}, nil
+}
+
+// saturateSub subtracts a set of coins from another. If the amount goes below
+// zero, it's set to zero.
+//
+// Example:
+// {2A, 3B, 4C} - {1A, 5B, 3D} = {1A, 4C}
+func saturateSub(coinsA sdk.Coins, coinsB sdk.Coins) sdk.Coins {
+	return coinsA.Sub(coinsA.Min(coinsB)...)
 }
--- x/shuttle/types/errors.go
+++ x/shuttle/types/errors.go
@@ -3,6 +3,9 @@ package types
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 var (
-	ErrAccountExists            = sdkerrors.Register(ModuleName, 2, "duplicate interchain account")
-	ErrInvalidProposalAuthority = sdkerrors.Register(ModuleName, 3, "invalid shuttle proposal authority")
+	ErrAccountExists            = sdkerrors.Register(ModuleName, 2, "interchain account already exists")
+	ErrAccountNotFound          = sdkerrors.Register(ModuleName, 3, "interchain account not found")
+	ErrChannelNotFound          = sdkerrors.Register(ModuleName, 4, "channel not found")
+	ErrInvalidProposalAuthority = sdkerrors.Register(ModuleName, 5, "invalid shuttle module proposal authority")
+	ErrMultihopUnsupported      = sdkerrors.Register(ModuleName, 6, "multihop channels are not supported")
 )
