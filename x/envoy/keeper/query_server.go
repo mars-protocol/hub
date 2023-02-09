54a3142d7a942a354e74fb10c00e4c11e127ae6f
larry <26318510+larry0x@users.noreply.github.com>
2023-02-09 13:37:41 +0000

    minor tweaks to the query methods

--- x/envoy/keeper/query_server.go
+++ x/envoy/keeper/query_server.go
@@ -6,6 +6,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
+	ibcchanneltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibccore "github.com/cosmos/ibc-go/v6/modules/core/exported"
+
 	"github.com/mars-protocol/hub/x/envoy/types"
 )
 
@@ -33,33 +36,6 @@ func (qs queryServer) Account(goCtx context.Context, req *types.QueryAccountRequ
 	return &types.QueryAccountResponse{Account: account}, nil
 }
 
-func (qs queryServer) AccountsFromICA(goCtx context.Context, req *types.QueryAccountsRequest) (*types.QueryAccountsResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	_, portID, err := qs.k.GetOwnerAndPortID()
-	if err != nil {
-		return nil, err
-	}
-
-	// the icaControllerKeeper does not provide a method to enumerate interchain
-	// accounts of a particular owner or port id. instead, we simply fetch _all_
-	// interchain accounts and filter them by the port id.
-	allAccounts := qs.k.icaControllerKeeper.GetAllInterchainAccounts(ctx)
-	accounts := []*types.AccountInfo{}
-	for _, account := range allAccounts {
-		if account.PortId == portID {
-			account, err := qs.queryAccount(ctx, account.ConnectionId, portID)
-			if err != nil {
-				return nil, err
-			}
-
-			accounts = append(accounts, account)
-		}
-	}
-
-	return &types.QueryAccountsResponse{Accounts: accounts}, nil
-}
-
 func (qs queryServer) Accounts(goCtx context.Context, req *types.QueryAccountsRequest) (*types.QueryAccountsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
@@ -68,7 +44,7 @@ func (qs queryServer) Accounts(goCtx context.Context, req *types.QueryAccountsRe
 		return nil, err
 	}
 
-	// we only need to fetch the channels with PortID equal to envoy module's ICA's
+	// we only need to fetch the channels with PortID equal to envoy module account's
 	// we filter the channels by provided method GetAllChannelsWithPortPrefix()
 	// note, this will probably return valid ICA channels for envoy module
 	// but we will compare PortID explicitly to keep things safe
@@ -95,10 +71,10 @@ func (qs queryServer) Accounts(goCtx context.Context, req *types.QueryAccountsRe
 func (qs queryServer) queryAccount(ctx sdk.Context, connectionID, portID string) (*types.AccountInfo, error) {
 	address, found := qs.k.icaControllerKeeper.GetInterchainAccountAddress(ctx, connectionID, portID)
 	if !found {
-		return nil, sdkerrors.ErrNotFound.Wrapf("envoy module-owned ICA: connection ID (%s)", connectionID)
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "envoy module-owned ICA: connection ID (%s)", connectionID)
 	}
 
-	// ordered channels are closed if a packet timesout:
+	// ordered channels are closed if a packet times out:
 	// https://github.com/cosmos/ibc-go/blob/v6.1.0/modules/core/04-channel/keeper/timeout.go#L173-L175
 	//
 	// in this case, this method call will fail. simply reopen a new channel by
@@ -108,12 +84,12 @@ func (qs queryServer) queryAccount(ctx sdk.Context, connectionID, portID string)
 	// interchain account exists but the channel is closed.")
 	channelID, found := qs.k.icaControllerKeeper.GetOpenActiveChannel(ctx, connectionID, portID)
 	if !found {
-		return nil, sdkerrors.ErrNotFound.Wrapf("ICA open active channel: connectionID (%s), portID (%s)", connectionID, portID)
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "ICA open active channel: connectionID (%s), portID (%s)", connectionID, portID)
 	}
 
 	channel, found := qs.k.channelKeeper.GetChannel(ctx, portID, channelID)
 	if !found {
-		return nil, sdkerrors.ErrNotFound.Wrapf("IBC channel: portID (%s) channelID (%s)", portID, channelID)
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "IBC channel: portID (%s) channelID (%s)")
 	}
 
 	connection, err := qs.k.channelKeeper.GetConnection(ctx, connectionID)
@@ -121,21 +97,7 @@ func (qs queryServer) queryAccount(ctx sdk.Context, connectionID, portID string)
 		return nil, err
 	}
 
-	return &types.AccountInfo{
-		Controller: &types.ChainInfo{
-			ClientId:     connection.GetClientID(),
-			ConnectionId: connectionID,
-			PortId:       portID,
-			ChannelId:    channelID,
-		},
-		Host: &types.ChainInfo{
-			ClientId:     connection.GetCounterparty().GetClientID(),
-			ConnectionId: connection.GetCounterparty().GetConnectionID(),
-			PortId:       channel.Counterparty.PortId,
-			ChannelId:    channel.Counterparty.ChannelId,
-		},
-		Address: address,
-	}, nil
+	return composeAccountInfo(address, connectionID, portID, channelID, connection, channel), nil
 }
 
 func (qs queryServer) queryAccountFromChannel(ctx sdk.Context, channelID, portID string) (*types.AccountInfo, error) {
@@ -154,6 +116,13 @@ func (qs queryServer) queryAccountFromChannel(ctx sdk.Context, channelID, portID
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "envoy module-owned ICA: connection ID (%s)", connectionID)
 	}
 
+	return composeAccountInfo(address, connectionID, portID, channelID, connection, channel), nil
+}
+
+func composeAccountInfo(
+	address, connectionID, portID, channelID string,
+	connection ibccore.ConnectionI, channel ibcchanneltypes.Channel,
+) *types.AccountInfo {
 	return &types.AccountInfo{
 		Controller: &types.ChainInfo{
 			ClientId:     connection.GetClientID(),
@@ -168,5 +137,5 @@ func (qs queryServer) queryAccountFromChannel(ctx sdk.Context, channelID, portID
 			ChannelId:    channel.Counterparty.ChannelId,
 		},
 		Address: address,
-	}, nil
+	}
 }
