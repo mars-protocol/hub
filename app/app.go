a657ac0fa3b4730da70d9354fc7b4d99c1b466d0
Jacob Gadikian <jacobgadikian@gmail.com>
2023-05-27 23:23:56 +0800

    continue to clean app.go

--- app/app.go
+++ app/app.go
@@ -145,6 +145,7 @@ import (
 )
 
 const (
+	NodeDir              = ".marsd"
 	AccountAddressPrefix = "mars"
 	Name                 = "mars"
 
@@ -164,9 +165,26 @@ const (
 	EnableSpecificProposals = ""
 )
 
+// These constants are derived from the above variables.
+// These are the ones we will want to use in the code, based on
+// any overrides above
 var (
+	// DefaultNodeHome default home directories for wasmd
+	DefaultNodeHome = os.ExpandEnv("$HOME/") + NodeDir
+
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = AccountAddressPrefix
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = AccountAddressPrefix + sdk.PrefixPublic
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixOperator
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixOperator + sdk.PrefixPublic
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixConsensus
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
 	// DefaultNodeHome the default home directory for the app daemon
-	DefaultNodeHome string
 
 	// ModuleBasics defines the module `BasicManager`, which is in charge of
 	// setting up basic, non- dependent module elements, such as codec
